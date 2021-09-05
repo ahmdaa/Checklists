9550d679e60e8d37c634af67aae6a908b32a5a92
Ahmed Abdalla <ahmedd.abdallaa@gmail.com>
2021-09-05 19:46:56 -0400

    Add text field to the add item screen where the user can input text for a new checklist item

--- Checklists/AddItemViewController.swift
+++ Checklists/AddItemViewController.swift
@@ -7,8 +7,16 @@
 
 import UIKit
 
-class AddItemViewController: UITableViewController {
-
+class AddItemViewController: UITableViewController, UITextFieldDelegate {
+    
+    @IBOutlet weak var textField: UITextField!
+    @IBOutlet var doneBarButton: UIBarButtonItem!
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        textField.becomeFirstResponder()
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -21,7 +29,35 @@ class AddItemViewController: UITableViewController {
     }
     
     @IBAction func done() {
+        print("Contents of the text field: \(textField.text!)")
+        
         navigationController?.popViewController(animated: true)
+    }
+    
+    // MARK:- Table View Delegates
+    // Disable selection for all rows
+    override func tableView(_ tableView: UITableView, willSelectRowAt indexPath: IndexPath) -> IndexPath? {
+        return nil
+    }
+    
+    // MARK:- Text Field Delegates
+    // Delegate method called when user changes input in text field
+    // Allows clicking the done button only if there is input in the text field
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        let oldText = textField.text!
+        let stringRange = Range(range, in: oldText)!
+        let newText = oldText.replacingCharacters(in: stringRange, with: string)
+        
+        doneBarButton.isEnabled = !newText.isEmpty
+        
+        return true
+    }
+    
+    // Delegate method called when user taps the text field's clear button
+    // Handles changing Done button state when user taps the clear button instead of deleting input they typed
+    func textFieldShouldClear(_ textField: UITextField) -> Bool {
+        doneBarButton.isEnabled = false
+        return true
     }
 
 }
