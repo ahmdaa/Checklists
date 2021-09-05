57f14791ee7bfd3258bf0ea684b422d1de2695e7
Ahmed Abdalla <ahmedd.abdallaa@gmail.com>
2021-09-05 13:43:56 -0400

    Add a data object ChecklistItem, populate table view using an array of that object and refactor code

---
+++ Checklists/ChecklistItem.swift
@@ -0,0 +1,13 @@
+//
+//  ChecklistItem.swift
+//  Checklists
+//
+//  Created by Ahmed Abdalla on 9/5/21.
+//
+
+import Foundation
+
+class ChecklistItem {
+    var text = ""
+    var checked = false
+}
