fd88ea7f9a2ec18a1104b463c53c7f641d0c6988
Ahmed Abdalla <ahmedd.abdallaa@gmail.com>
2021-09-14 00:40:54 -0400

    Add data storage and retrieval through .plist encoding and decoding in app's documents folder

--- Checklists/ChecklistViewController.swift
+++ Checklists/ChecklistViewController.swift
@@ -16,27 +16,11 @@ class ChecklistViewController: UITableViewController, ItemDetailViewControllerDe
         
         navigationController?.navigationBar.prefersLargeTitles = true
         
-        let item1 = ChecklistItem()
-        item1.text = "Walk the dog"
-        items.append(item1)
-
-        let item2 = ChecklistItem()
-        item2.text = "Brush my teeth"
-        item2.checked = true
-        items.append(item2)
-
-        let item3 = ChecklistItem()
-        item3.text = "Learn iOS development"
-        item3.checked = true
-        items.append(item3)
-
-        let item4 = ChecklistItem()
-        item4.text = "Soccer practice"
-        items.append(item4)
-
-        let item5 = ChecklistItem()
-        item5.text = "Eat ice cream"
-        items.append(item5)
+        // Load items
+        loadChecklistItems()
+        
+        // print("Documents folder is \(documentsDirectory())")
+        // print("Data file path is \(dataFilePath())")
     }
     
     // MARK:- Actions
@@ -57,7 +41,7 @@ class ChecklistViewController: UITableViewController, ItemDetailViewControllerDe
     
     // MARK:- Table View Data Source
     // override because TableViewController already has the methods
-    // _ means no external parameter name
+    // _ means no external parameter name.
     // External parameter name - Internal parameter name
     // numberOfRowsInSection   - section
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -85,14 +69,16 @@ class ChecklistViewController: UITableViewController, ItemDetailViewControllerDe
         }
         
         tableView.deselectRow(at: indexPath, animated: true)
+        saveChecklistItems()
     }
     
-    // Enable swipe to delete
+    // Enable swipe to delete.
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
         items.remove(at: indexPath.row)
         
         let indexPaths = [indexPath]
         tableView.deleteRows(at: indexPaths, with: .automatic)
+        saveChecklistItems()
     }
     
     // MARK:- Add Item View Controller Delegates
@@ -106,14 +92,15 @@ class ChecklistViewController: UITableViewController, ItemDetailViewControllerDe
         
         let indexPath = IndexPath(row: newRowIndex, section: 0)
         
-        // Store new row in temporary array
+        // Store new row in temporary array.
         let indexPaths = [indexPath]
         
-        // Insert new row (or rows) in the table view
-        // .automatic parameter animates the insertion
+        // Insert new row (or rows) in the table view.
+        // .automatic parameter animates the insertion.
         tableView.insertRows(at: indexPaths, with: .automatic)
         
         navigationController?.popViewController(animated: true)
+        saveChecklistItems()
     }
     
     func itemDetailViewController(_ controller: ItemDetailViewController, didFinishEditing item: ChecklistItem) {
@@ -124,6 +111,7 @@ class ChecklistViewController: UITableViewController, ItemDetailViewControllerDe
             }
         }
         navigationController?.popViewController(animated: true)
+        saveChecklistItems()
     }
     
     // MARK:- Navigation
@@ -140,6 +128,55 @@ class ChecklistViewController: UITableViewController, ItemDetailViewControllerDe
             }
         }
     }
-
+    
+    // MARK:- Data Storage
+    // Returns URL path to the Documents folder for the app.
+    // iOS uses URLS to refer to files in its filesystem.
+    // Format: file://URL
+    func documentsDirectory() -> URL {
+        let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+        return paths[0]
+    }
+    
+    // Returns full URL path to the Property List (.plist) file for storing data.
+    func dataFilePath() -> URL {
+        documentsDirectory().appendingPathComponent("Checklists.plist")
+    }
+    
+    // Serialization method
+    // Retrieves the contents of the items array, converts it to a block of binary data, and then writes this data to a file.
+    func saveChecklistItems() {
+        // Create an instance of PropertyListEncoder which will encode the items array, and all the ChecklistItems in it, into a binary data format that can be written to a file.
+        let encoder = PropertyListEncoder()
+        
+        // The do keyword sets up a block of code to catch Swift errors. Swift handles errors under certain conditions by throwing an error. In such cases, you need a block of code to catch the error and to handle it. The do keyword indicates the start of such a block.
+        do {
+            // The encoder you created earlier is used to try to encode the items array. The encode method throws a Swift error if it is unable to encode the data for some reason — for example, the data is not in the expected format, or it is corrupted etc. The try keyword indicates that the call to encode can fail and if that happens, that it will throw an error.
+            let data = try encoder.encode(items)
+            
+            // If the data constant was successfully created by the call to encode in the previous line, then you write the data to a file using the file path returned by a call to dataFilePath(). Note that the write method also can throw an error.
+            try data.write(to: dataFilePath(), options: Data.WritingOptions.atomic)
+        // The catch statement indicates the block of code to be executed if an error was thrown by any line of code in the enclosing do block.
+        } catch {
+            // Handle the caught error. Simply print out an error message. Local variable error is populated automatically by Swift and it contains the error thrown by one of the statements within the do block.
+            print("Error encoding item array:\(error.localizedDescription)")
+        }
+    }
+    
+    func loadChecklistItems() {
+        // Get URL path to data file and store in path.
+        let path = dataFilePath()
+        // Load the contents of Checklists.plist into a new Data object. The try? command attempts to create the Data object, but returns nil if it fails.
+        if let data = try? Data(contentsOf: path) {
+            // Create a decoder instance.
+            let decoder = PropertyListDecoder()
+            do {
+                // Load the saved data back into items using the decoder's decode method. The only item of interest here would be the first parameter passed to decode. The decoder needs to know what type of data will be the result of the decode operation and you let it know by indicating that it will be an array of ChecklistItem objects.
+                items = try decoder.decode([ChecklistItem].self, from: data)
+            } catch {
+                print("error decoding item array: \(error.localizedDescription)")
+            }
+        }
+    }
 }
 
